b502c5880258fcaae116425047b0563fb4e5786e
FerTorres13 <fernandatorresquimi@gmail.com>
2019-11-25 03:28:05 +0100

    cambiando cierta estructura en Nuevo Juego

--- QuienQuiereSerMillonario/src/menu/MenuPrincipal.java
+++ QuienQuiereSerMillonario/src/menu/MenuPrincipal.java
@@ -102,6 +102,8 @@ public class MenuPrincipal {
                                 }
                                 nuevoJuego.setCompañero(matEstudianteCompañero); //se asigna al compañero al nuevo juego
                                 nuevoJuego.setPreguntas(listaPreguntas); //se setea la lista de preguntas
+                                System.out.println("Presione una tecla para continuar");
+                                String avanzar = sc.next();
                                 nuevoJuego.iniciar(); //se inicia el nuevo juego
                           }
                       }
